14cdeae97fdff1d12a6fb083477cb22a2911d47f
Dungeoner <Dungeoner@DESKTOP-4SNPBLN>
2020-02-17 21:49:10 +0200

    Add project files.

---
+++ ArrayFill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_Deserialization
+{
+    class ArrayFill
+    {
+        public static int[] Filling(int size)
+        {
+            var array = new int[size];
+            var rnd = new Random(100);
+            for(var i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next();
+            }
+
+            return array;
+        }
+    }
+
+}
